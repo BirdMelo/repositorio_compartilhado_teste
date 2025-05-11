937e268535d31df966368a4bf603b655ad100c42
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2025-05-11 14:59:31 -0300

    projeto terminado

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const button = document.querySelector("#button");
+const input = document.querySelector("#numero");
+
 
 button.addEventListener("click", () => {
-    console.log("Button clicked!");
-});
+    const numero  = input.value;
+
+    if (numero === "") {
+        alert("Por favor, insira um número.");
+        return;
+    } else if (numero < 0) {
+        alert("Por favor, insira um número maior ou igual a 0.");
+        return;
+    } else if (numero % 2 == 0) {
+        alert(`O número ${numero} é par.`);
+    } else if (numero % 2 != 0) {
+        alert(`O número ${numero} é ímpar.`);
+    } else {
+        alert("Erro não detectado.");
+    }
+})
